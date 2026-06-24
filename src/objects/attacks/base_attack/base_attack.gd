4c3fc4d3cbbe2c9311c60e147cd628a5ff756be0
Ishidawg <willianscagol@gmail.com>
2026-06-24 20:24:50 -0300

    general shit

--- src/objects/attacks/base_attack/base_attack.gd
+++ src/objects/attacks/base_attack/base_attack.gd
@@ -24,15 +24,16 @@ func run_animation() -> void:
 
 func execute() -> void:
 	run_animation()
-	
+
 	var effective_damage = damage * multiplier
-	
+
 	await Utils.sleep(pre_step_duration)
-	var step_damage = effective_damage / step
-	
+	var step_damage = effective_damage / step # Tem que cuidar aqui
+	# Caso o step seja 0, o godot vai crashar.
+
 	if effective_damage <= 0:
 		return
-	
+
 	for i in range(1, step+1):
 		if i == step:
 			var difference = effective_damage - (step_damage * step)
