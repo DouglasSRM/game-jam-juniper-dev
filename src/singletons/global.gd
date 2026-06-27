7f7cbf56468f513536ffe4dd334b288868056073
Douglas Seramim <douglasfseramim@gmail.com>
2026-06-27 00:55:56 -0300

    feat: fixes

--- src/singletons/global.gd
+++ src/singletons/global.gd
@@ -14,6 +14,6 @@ var current_weapon: PackedScene = null
 
 var potions_in_inventory: int = 3
 
-var health_to_append: float = 2
+var health_to_append: float = 5
 
 var teve_dialogos_dungeon_entrance: bool = false
