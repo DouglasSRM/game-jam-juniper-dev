23e43522bbefcd3720a22f715747038d45182c9a
Ishidawg <willianscagol@gmail.com>
2026-06-27 00:27:15 -0300

    add stuff to hub 1 and hub 2 - DOUGLA ADD COLISION PLS

--- src/scenes/dungeon_floor_1/dungeon_floor_1.gd
+++ src/scenes/dungeon_floor_1/dungeon_floor_1.gd
@@ -5,9 +5,16 @@ var timer_water_animation: float = 0.
 @onready var agua_3: TileMapLayer = $TileMaps/Agua3
 @onready var agua_2: TileMapLayer = $TileMaps/Agua2
 
+@onready var boss_label: Label = $BossLabel
+@onready var parchment: Sprite2D = $Parchment
+
+@onready var interactable: Interactable = $Interactables/Interactable
+
 func _ready() -> void:
 	agua_2.visible = false
 	agua_3.visible = false
+	boss_label.visible = false
+	parchment.visible = true
 
 func update_water() -> void:
 	if (not agua_2.visible) and (not agua_3.visible):
@@ -37,4 +44,6 @@ func _on_trigger_next_scene_body_entered(body: Node2D) -> void:
 		go_to_next_scene()
 
 func show_boss_text() -> void:
-	pass
+	interactable.visible = false
+	boss_label.visible = true
+	parchment.visible = false
